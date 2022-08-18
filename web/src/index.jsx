ff7618eaaf220356540f8ed1c77b5f547e88daba
CouncilDataProjectServiceAccount <councildataproject@gmail.com>
2022-08-18 23:13:40 +0000

    Initial commit

---
+++ web/src/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App, AppConfigProvider } from "@councildataproject/cdp-frontend";
+
+import "@councildataproject/cdp-frontend/dist/index.css";
+
+const config = {
+    firebaseConfig: {
+        options: {
+            projectId: "cdp-long-beach-49323fe9",
+        },
+        settings: {},
+    },
+    municipality: {
+        name: "Long Beach",
+        timeZone: "America/Los_Angeles",
+        footerLinksSections: [],
+    },
+}
+
+ReactDOM.render(
+    <div>
+        <AppConfigProvider appConfig={config}>
+            <App />
+        </AppConfigProvider>
+    </div>,
+    document.getElementById("root")
+);
