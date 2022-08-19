b9c6f50ea5f05ea4341a120e469720b61afa5162
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2022-08-18 17:56:25 -0700

    Add ignore minutes items

--- python/cdp_long_beach_backend/scraper.py
+++ python/cdp_long_beach_backend/scraper.py
@@ -18,6 +18,22 @@ def get_events(
     scraper = LegistarScraper(
         client="longbeach",
         timezone="America/Los_Angeles",
+        ignore_minutes_item_patterns=[
+            "VIA TELECONFERENCE PURSUANT TO AB 361",
+            "certify that the agenda was posted not less than 72 hours",
+            "The agenda and supporting documents are available on the Internet",
+            "IN-PERSON/VIRTUAL HYBRID CITY COUNCIL MEETING",
+            "PURSUANT TO AB 361",
+            "Opportunity to address the City Council",
+            "VIA TELECONFERENCE",
+            "JOIN VIA ZOOM",
+            "TO PROVIDE PUBLIC COMMENT IN THE ZOOM MEETING",
+            "NOTICE TO THE PUBLIC",
+            "Opportunity is given to those members of the public",
+            "page break",
+            "REVISED",
+            "PAGE BREAK",
+        ],
     )
 
     return scraper.get_events(begin=from_dt, end=to_dt)
